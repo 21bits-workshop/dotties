2501838ddb6253498a7a9aca2c64b5f3e748bf61
Takomo <21bits.workshop@gmail.com>
2025-12-10 22:11:50 -0500

    Added config files.

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "Verf/deepwhite.nvim" },
+  { "ramojus/mellifluous.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "mellifluous",
+    },
+  },
+}
